605cfeb6c8e52a5342dc0c727e3b937083238c8d
tianxingyzxq <562479011@qq.com>
2016-05-29 23:24:54 +0800

    commit 1.

--- testgit.cpp
+++ testgit.cpp
@@ -4,6 +4,7 @@ int int main(int argc, char *argv[])
 {
     std::cout << "hello world." << std::endl;
     printf("%s\n", "printf");
-    add a new line.
+    add a new line;
+    add one line;
     return 0;
 }
